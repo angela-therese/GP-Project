9164ed6be34878e78187d20700b704cfdcd2ffc9
User-42 <a.t.iafrate@gmail.com>
2022-04-04 18:19:24 -0400

    nav bar modified

--- client/gp-project/src/components/Nav/Nav.js
+++ client/gp-project/src/components/Nav/Nav.js
@@ -1,11 +1,13 @@
-import React, { Component, useContext, useEffect } from "react"
+import React, { useEffect,  useState }  from "react"
+
 // import { UserContext } from "../UsersProvider"
 import { Nav } from "react-bootstrap"
 import { Button } from "react-bootstrap"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, Link} from "react-router-dom"
 import "./Nav.css"
 import Home from '../../images/home-icon.png'
 import ProfilePic from '../../images/angieGlasses.jpg'
+import Logo from '../../images/GrowPath.png'
 
 
 
@@ -19,12 +21,41 @@ export const NavBar = () => {
         navigate(`/`)
     }
 
+    
+    const [growpathUser, setGrowPathUser] = useState({});
+    useEffect(() => {
+        const growpathUser = JSON.parse(localStorage.getItem('growpathUser'));
+        if (growpathUser){
+            setGrowPathUser(growpathUser)
+        }
+    }, {})
+
+  
+
+
     return (
         <>
             <Nav defaultActiveKey="/home" 
             className="nav-menu">
-                <img width="5%" alt="profile_picture" src={ProfilePic}/>
+                
+                <div className="left-nav">
+                <img className="logo-nav-image" width="60px" alt="profile_picture" src={Logo}/>
+                <Link className="header-text" to={`/user/${growpathUser.id}`}><h1>GrowPath</h1></Link>
+                
+                
+             
+                
+                </div>
+                
+                <div className="right-nav">
+                <img className="profile-image" width="7%" height="7%" alt="profile_picture" src={ProfilePic}/>
+                
+                <h3>{growpathUser.firstName} {growpathUser.lastName}</h3>
                 <button className="btnLogout" onClick={handleLogout}>Logout</button>
+                
+               
+                </div>
+                
             </Nav>
 
             {/* </section> */}
